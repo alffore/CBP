58003fbe763dc59859d0e290a8d2422e8be02a99
alfonso <alfonso@localhost>
2013-11-17 12:42:16 -0600

    Se incluyen los elementos básicos del proyecto

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: alfonso
+ *
+ * Created on 17 de noviembre de 2013, 12:37 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
